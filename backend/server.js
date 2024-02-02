50a01dc0b5e49df63438615681098b14dc2b9a09
arthdarji352 <arthdarji20@gmail.com>
2024-02-02 18:19:43 +0530

    add protect route and send Message route

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server running on port ${PORT}`);
