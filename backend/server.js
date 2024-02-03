4d022b818a58f096b1f4d47d2ec7deb5c85a279e
arthdarji352 <arthdarji20@gmail.com>
2024-02-03 13:47:53 +0530

    add get all user route but not add login user

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server running on port ${PORT}`);
