3d6f09413ca898da149805ea60ebcb205dae9534
arthdarji352 <arthdarji20@gmail.com>
2024-02-07 18:05:31 +0530

    add login,Signup and Logout functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,28 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 const App = () => {
+  const { authUser } = useAuthContext();
   return (
-    <>
-      <div className="p-4 h-screen flex items-center justify-center">
-        <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-            // element={authUser ? <Home /> : <Navigate to={"/login"} />}
-          />
-          <Route
-            path="/login"
-            element={<Login />}
-            // element={authUser ? <Navigate to="/" /> : <Login />}
-          />
-          <Route
-            path="/signup"
-            element={<SignUp />}
-            // element={authUser ? <Navigate to="/" /> : <SignUp />}
-          />
-        </Routes>
-        <Toaster />
-      </div>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
   );
 };
 
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
   const signup = async ({
     fullName,
@@ -38,8 +40,11 @@ const useSignup = () => {
       if (data.error) {
         throw new Error(data.error);
       }
-      console.log(data);
-      toast.success("Sign Up");
+
+      //local storage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      //context
+      setAuthUser(data);
     } catch (error) {
       toast.error(error.message);
     } finally {
