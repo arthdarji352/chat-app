0be37eb9918bad4cbcc9f0d81e0bc27f5bd20339
arthdarji352 <arthdarji20@gmail.com>
2024-02-08 15:12:40 +0530

    add socket.io ,real time messages

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -16,7 +17,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log(`Server running on port ${PORT}`);
 });
